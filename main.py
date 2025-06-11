67f392cb3c6a766d21557a48c89c6accd5ee43ed
Chaitanya Vaddisriram <53746340+chaitanyavaddi@users.noreply.github.com>
2025-06-11 19:38:20 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+library = []
+
+@app.post("/book/add")
+def add_book(name, desc):
+    library.append({"name": name, "desc": desc})
+    return library
+
+@app.put("/book/update")
+def update_book(name, updated_desc):
+    for book in library:
+        if book['name'] == name:
+            book["desc"] = updated_desc
+            return library
+    else:
+        return "No book found"
+
+
+@app.delete("/book/delete")
+def delete_book():
+    return "Hello"
+
+@app.get("/book/get")
+def get_book():
+    return "Hello"
