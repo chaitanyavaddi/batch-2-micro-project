432088a27e771bc7722d29fd11c168ce7e38f2d0
Chaitanya Vaddisriram <53746340+chaitanyavaddi@users.noreply.github.com>
2025-06-16 19:41:14 +0530

    simple frontend

--- main.py
+++ main.py
@@ -11,17 +11,27 @@ database = create_client(supabase_url, supabase_api_key)
 #Read
 # result = database.table("app_users").select('*').execute()
 
+@app.post("/login")
+def login(username, password):
+    #get db record with username
+    result = database.table("app_users").select("*").eq('username', username).eq('password', password).execute()
+    if len(result.data) > 0:
+        return True
+    else:
+        return False
+    
+
 @app.get("/users")
 def read_users():
-    result = database.table("app_users").select('*').execute()
+    result = database.table('app_users').select('*').execute()
     return result.data
 
 @app.get('/create')
-def create_user(name, age, password):
+def create_user(name, age, pass2):
     result = database.table('app_users').insert({
         'name': name,
         'age': age,
-        'password': password
+        'password': pass2
     }).execute()
 
     return result.data
